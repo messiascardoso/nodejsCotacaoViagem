016a0f43824eed478cdc8a96aaaf909c33913c54
messiascardoso <messiascardoso@gmail.com>
2021-04-20 00:20:24 -0300

    Calculo de melhor rota OK

--- controllers/cotacao.js
+++ controllers/cotacao.js
@@ -1,9 +1,38 @@
 module.exports = function(app) {
     const controller = {};
+    var fs = require('fs').promises;
     
-    controller.getCotacao = function(req, res) {
-        console.log('controller.getCotacao')
+    controller.getCotacao = async function(req, res) {
+        const fileContent = await fs.readFile(__dirname.replace('controllers', '')+'/routes.csv');
+        const routesCsv =  convertCsvToJson(fileContent.toString())
+        const params = {} 
+        params.from = req.params[0]
+        params.to = req.params[1]
+        const bestToRoute = calcRoute(params, routesCsv)
+        res.json(bestToRoute)
+    }
+
+    function convertCsvToJson(fileString) {
+      fileString = (fileString.replace(/\r/g,'').split('\n'));
+      fileString.pop()
+      return fileString.map(row=> {
+            const item = row.split(',')
+            return item ? { from: item[0], to:item[1], price: Number(item[2])} : '';
+        })
     }
     
+    function calcRoute(params, routesCsv ) {
+       const filterByFrom =  routesCsv.filter(rote =>  rote.from.toLowerCase() === params.from.toLowerCase())
+       const  bestFromRoute = filterByFrom.filter( (y, index, array)=>{ 
+         return y.price === Math.min(...array.map(x => x.price))
+       })[0]
+
+       const filterByTo =  routesCsv.filter(rote =>  rote.to.toLowerCase() === bestFromRoute.to.toLowerCase())
+       const  bestToRoute = filterByTo.filter( (y, index, array)=>{ 
+         return y.price === Math.min(...array.map(x => x.price))
+       })
+      return bestToRoute;
+    }
+
     return controller; 
 }
--- routes/cotacao.js
+++ routes/cotacao.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     const controller = app.controllers.cotacao;
-    app.route('/cotacao')
+    app.route(/^\/quote\/(\w+)\/(\w+)?$/)
             .get(controller.getCotacao)
             
 }
